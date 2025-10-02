e35b5d6fbc4cf9fca369a425e538505711586056
Asi-05 <you@example.com>
2025-10-02 23:27:11 +0200

    comments hinzugefügt

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ def start_menu(): #definiert die Funktoin Start mit Namen start_menu
     print("4) Edit")
     print("5) Programm beenden")
     
-    choice = input("Bitte wähle eine Option (1-4): ")
-    return choice #choice wird zum auswahl 
+    choice = input("Bitte wähle eine Option (1-5): ")
+    return choice #choice wird gemäss eingabe vom User wiedergegeben
 
 #Hauptprogramm mit Schleife
 while True:
-    auswahl = start_menu() #auswahl bekommt den Wert von Choice
+    auswahl = start_menu() #funktion (start_menu) wird abgerufen, return choice gibt wert an bspw: 3
 
     if auswahl == "1":
         print("Einnahmen hinzufügen")
     elif auswahl == "2":
         print("Ausgaben hinzufügen")
     elif auswahl == "3":
-        print("Überischt anzeigen")
+        print("Übersicht anzeigen")
     elif auswahl == "4":
         print("Edit")
     elif auswahl == "5":
