e9b54b0bbe878126ed5b21080695bb88025177db
Filmon Samy <filmon.samy@students.fhnw.ch>
2025-12-13 19:16:11 +0100

    kommentare hinzugefügt

--- main/einnahmen_ausgaben/ausgaben_hinzufuegen.py
+++ main/einnahmen_ausgaben/ausgaben_hinzufuegen.py
@@ -1,8 +1,8 @@
 from datetime import datetime  
-from datei_kontrollieren.datei_pruefen import *
-from datum_eingabe.datum_eingabe import *
+from datei_kontrollieren.datei_pruefen import *     #importiert die funktion zur Überprüfung/Erstellung der CSV-Datei
+from datum_eingabe.datum_eingabe import *           #importiert funktion datum eingabe mit kontrolle
 import csv
-from kategorie_auswahl.kategorieauswahl import *
+from kategorie_auswahl.kategorieauswahl import *    #Importiert Auswahl für Kategorie
 
 
 
@@ -15,31 +15,33 @@ def ausgaben_hinzufuegen():
     print('x) Zurück zum Hauptmenu')
 
     while True:                                             #Betrag abfragen
-        eingabe = input("Betrag (CHF): ").strip()
+        eingabe = input("Betrag (CHF): ").strip()           #Fragt den nutzer nach dem Betrag und strip entfernt die Leerzeichen am Anfang und Ende
 
-        if eingabe.lower() == 'x':
+        if eingabe.lower() == 'x':                          #schaut ob der User x oder X eingegeben hat, um zurückzukehren
             return                                          #zurück zum Hauptmenu
 
-        try:
+        try:                                                #versucht die eingabe in ein float umzuwandeln
             betrag = float(eingabe)
             
-            if betrag < 0:
-                print('Bitte positiven Betrag eingeben.')
+            if betrag < 0:                                  #überprüft ob die eingabe eine negative zahl ist
+                print('Bitte positiven Betrag eingeben.')   #gibt diese Rückmeldung bei negativer Zahl und kehrt dann zur Betrag Abfrage zurück
                 continue
             
-            break
-        except ValueError:
+            break                                           #wenn umwandlung in float und postiver betrag dann wird die schleife beendet
+        except ValueError:                                  #wird ausgelöst wenn die eingabe nicht in eine float umgewnadelt werden kann
             print("Bitte gültigen Betrag eingeben!")
 
     datum = datum_eingabe()                                 #Datum eingabe Funktion wird aufgerufen
 
-    kategorie = kategorieauswahl_ausgabe()
+    kategorie = kategorieauswahl_ausgabe()                  #ruft die kategorien zum typ ausgabe ab und verlangt eingabe vom user
 
+                                                            #abschnitt zum in der csv datei speichern
     '''In CSV Datei speichern'''
-    with open(DATEI, 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([datum, betrag, 'Ausgabe',kategorie])
+                                                            #öffnet die datei (DATEI) im Anhänge Modus (a, append), newline= verhindert leere Zeichen
+    with open(DATEI, 'a', newline='') as file:              #with open damit die datei am schluss automatisch geschlossen wird
+        writer = csv.writer(file)                           #erstellt eine csv-wirter objekt für die geöffnete datei
+        writer.writerow([datum, betrag, 'Ausgabe',kategorie]) #schreibt eine neue zeile mit den erfassten date: datum, betrag, ausgabe, kateogire
 
-    print(f'\nAusgabe von {betrag:.2f} CHF {kategorie} wurde gespeichert. \n')
+    print(f'\nAusgabe von {betrag:.2f} CHF {kategorie} wurde gespeichert. \n') #der Betrag wird auf 2 dezimalen formatiert und user erhält bestätigung von kategorie und betrag
 
 
